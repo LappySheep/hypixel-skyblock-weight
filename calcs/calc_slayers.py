ecef37594971e5d89ea64d010fbe41fcccff4bb8
LappySheep <anonymous66634@yahoo.co.uk>
2021-05-16 18:13:08 +0100

    Update calc_slayers.py

--- calcs/calc_slayers.py
+++ calcs/calc_slayers.py
@@ -1,28 +1,40 @@
 import asyncio
+from calcs.calc_slayerscore import calc_slayerscore
 
 
-async def new_slayer(s):
-  if s > 25000000:
-    return 7985000 + ((s - 25000000) * 0.1)
-  elif s > 12500000: # 7.985mil @ 25mil xp
-    return 6110000 + ((s - 12500000) * 0.25)
-  elif s > 5000000: # 6.11mil @ 12.5mil xp
-    return 3110000 + ((s - 5000000) * 0.4)
-  elif s > 1500000: # 3.11mil @ 5mil xp
-    return 1185000 + ((s - 1500000) * 0.55)
-  elif s > 700000: # 1.185m @ 1.5mil xp
-    return 625000 + ((s - 700000) * 0.7)
-  elif s > 200000: # 625k @ 700k xp
-    return 200000 + ((s - 200000) * 0.85)
-  else: # x <= 200000
-    return s
+def effective_int(integer_part):
+  t = 0
+  for k in range(integer_part):
+    k += 1
+    calc = (k**2 + k) * 0.79**k
+    t += calc
+  t = round(1000000 * t / 17, 2)
+  return t
+
+
+def actual(int_part):
+  return ((int_part**3 / 6) + (int_part**2 / 2) + (int_part / 3))*100000
+
+
+async def new_slayer(score, xp):
+  step1 = int(score) # e.g. 1.5mil xp -> ~3.55
+  step2 = effective_int(step1) # e.g. 3 -> 788.5k
+  step3 = actual(step1) # e.g. slayer score 3 -> 1mil xp
+  step4 = xp - step3 # distance -> 1.5mil - 1m = 500k xp
+  step5 = step4 * 0.79**step1 # int is 3, 0.79**3 = ??, multiplied by 500k
+  step6 = step2 + step5 # 788.5k + [reduced 500k] = oiufods
+  return step6 # return step 6
 
 
 async def calc_slayers(zombie, spider, wolf):
-  zombie2 = await new_slayer(zombie)
-  spider2 = await new_slayer(spider)
-  wolf2 = await new_slayer(wolf)
+  zombie_score = await calc_slayerscore(zombie)
+  spider_score = await calc_slayerscore(spider)
+  wolf_score = await calc_slayerscore(wolf)
 
-  individual = zombie2/6250 + spider2/4500 + wolf2/2250
-  extra = (zombie + spider + wolf) / 150000
-  return (individual + extra), (zombie + spider + wolf) # base + extra
+  zombie2 = await new_slayer(zombie_score, zombie)
+  spider2 = await new_slayer(spider_score, spider)
+  wolf2 = await new_slayer(wolf_score, wolf)
+
+  individual = zombie2/6000 + spider2/4500 + wolf2/2000
+  extra = (zombie + spider + (2*wolf)) / 225000
+  return (individual + extra), (zombie + spider + wolf)
