91eef7e940df07c0ca0c109d1469a75f2be6512e
LappySheep <lappysheep@gmail.com>
2021-07-19 22:02:08 +0100

    v1.717
    more readable, weight shifts

--- calcs/calc_slayers.py
+++ calcs/calc_slayers.py
@@ -1,29 +1,38 @@
 import asyncio
 from calcs.calc_slayerscore import calc_slayerscore
 
+depreciation_scaling = [0.82030106128, 0.8248013266, 0.83460185724, 0.8507026532]
 
-def effective_int(integer_part):
-  t = 0
+
+def effective_int(integer_part, ind):
+  scaling = depreciation_scaling[ind]
+  total = 0
   for k in range(integer_part):
     k += 1
-    calc = (k**2 + k) * 0.79**k
-    t += calc
-  t = round(1000000 * t / 17, 2)
-  return t
+    calc = (k**2 + k) * scaling**k
+    total += calc
+  total = round(1000000 * total * (0.05/scaling), 2)
+  return total
 
 
 def actual(int_part):
   return ((int_part**3 / 6) + (int_part**2 / 2) + (int_part / 3))*100000
 
 
-async def new_slayer(score, xp):
-  step1 = int(score) # e.g. 1.5mil xp -> ~3.55
-  step2 = effective_int(step1) # e.g. 3 -> 788.5k
-  step3 = actual(step1) # e.g. slayer score 3 -> 1mil xp
-  step4 = xp - step3 # distance -> 1.5mil - 1m = 500k xp
-  step5 = step4 * 0.79**step1 # int is 3, 0.79**3 = ??, multiplied by 500k
-  step6 = step2 + step5 # 788.5k + [reduced 500k] = oiufods
-  return step6 # return step 6
+def new_slayer(score, xp, ind): # example uses depreciation_scaling = 0.85
+  # e.g. 1.5mil xp -> ~3.55 -> 3
+  score_rdown = int(score)
+  # e.g. 3 -> 788.5k
+  effective_rdown = effective_int(score_rdown, ind)
+  # e.g. slayer score 3 -> 1mil xp
+  actual_rdown = actual(score_rdown)
+  # distance -> 1.5mil - 1m = 500k xp
+  distance = xp - actual_rdown
+  # int is 3, 0.85**3 = 0.614125, multiplied by 500k
+  effective_distance = distance * depreciation_scaling[ind]**score_rdown
+  # 788.5k + [reduced 500k] = oiufods
+  total_effective = effective_rdown + effective_distance
+  return total_effective
 
 
 async def calc_slayers(zombie, spider, wolf, enderman):
@@ -32,11 +41,11 @@ async def calc_slayers(zombie, spider, wolf, enderman):
   wolf_score = await calc_slayerscore(wolf)
   eman_score = await calc_slayerscore(enderman)
 
-  zombie2 = await new_slayer(zombie_score, zombie)
-  spider2 = await new_slayer(spider_score, spider)
-  wolf2 = await new_slayer(wolf_score, wolf)
-  eman2 = await new_slayer(eman_score, enderman)
+  zombie2 = new_slayer(zombie_score, zombie, 0)
+  spider2 = new_slayer(spider_score, spider, 1)
+  wolf2 = new_slayer(wolf_score, wolf, 2)
+  eman2 = new_slayer(eman_score, enderman, 3)
 
-  individual = zombie2/6000 + spider2/4500 + wolf2/2000 + eman2/925
-  extra = (zombie + spider + (2*wolf) + (2*enderman)) / 240000
+  individual = zombie2/7000 + spider2/4800 + wolf2/2200 + eman2/1000
+  extra = (zombie + 1.6*spider + 3.6*wolf + 10*enderman) / 900000
   return (individual + extra), (zombie + spider + wolf + enderman)
